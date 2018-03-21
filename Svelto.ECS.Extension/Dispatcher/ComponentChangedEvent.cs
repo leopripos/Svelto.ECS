223575a883539253bc611f390dbb9deee5103dc4
Leo Pripos Marbun <leopripos@gmail.com>
2018-03-21 10:18:06 +0700

    Add Dispatcher extension for component changed

---
+++ Svelto.ECS.Extension/Dispatcher/ComponentChangedEvent.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Svelto.ECS.Extension
+{
+    public class ComponentChangedEvent<T>
+    {
+        public ComponentChangedEvent(int entityId, T component)
+        {
+            _entityId = entityId;
+            _component = component;
+        }
+
+        public void Dispatch()
+        {
+            _subscribers?.Invoke(_entityId, _component);
+        }
+
+        public void Notify(Action<int, T> action)
+        {
+            _subscribers += action;
+        }
+
+        public void StopNotify(Action<int, T> action)
+        {
+            _subscribers -= action;
+        }
+
+        readonly int _entityId;
+        readonly T _component;
+
+        event Action<int, T> _subscribers;
+    }
+}
